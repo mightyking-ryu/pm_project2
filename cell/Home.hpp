c04d45e875cfb7e57d50d48a4e4282da83bf8faf
mightyking-ryu <dongwooryu@snu.ac.kr>
2024-06-06 20:45:15 +0900

    implemented Home.hpp

--- cell/Home.hpp
+++ cell/Home.hpp
@@ -1,27 +1,21 @@
 //////////     TODO     ////////////////////////////////////
 // Declare your Home class
 
+#pragma once
+#include "cell/Cell.hpp"
 
+class Home : public Cell
+{
+public:
+    Home(Map* map, int row, int col, int targetNumber);
 
+    int targetNumber;
 
+    AttrType GetAttr() const override;
+    ColorPair GetColorPair() const override;
+    char GetIcon() const override;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    HomeCheckResult Check() const;
+};
 
 //////////   TODO END   ////////////////////////////////////
